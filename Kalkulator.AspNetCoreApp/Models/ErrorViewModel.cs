72a3378ae9a5a4476e2f7e6f42035f73b5d2236e
jakub <jakub@HOME>
2022-07-12 17:17:08 +0200

    Add project files.

---
+++ Kalkulator.AspNetCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kalkulator.AspNetCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
